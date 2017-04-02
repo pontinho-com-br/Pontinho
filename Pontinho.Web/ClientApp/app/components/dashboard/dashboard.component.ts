53c5d4c9d2f17b63eafde389fc7792951a9b7538
Thiago Passos <thiago@passos.com.au>
2017-04-02 12:20:51 -0300

    adding initial layout

--- Pontinho.Web/ClientApp/app/components/dashboard/dashboard.component.ts
+++ Pontinho.Web/ClientApp/app/components/dashboard/dashboard.component.ts
@@ -1,45 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 var $;
+var Chartist;
 
 @Component({
     templateUrl: 'dashboard.component.html'
 })
 
 export class DashboardComponent implements OnInit {
-    charts = [];
-    model = {};
+    charts: any = {};
+    model = { totalCompetitions: 10, totalMatches: 10, totalRounds: 10, matchesWon: 5, roundsWon: 5, roundsSnapped: 5 };
 
     constructor() { }
 
     ngOnInit() {
-        // var _this = this;
+        var _this = this;
         // this.competitionService.getDashboard().success(function (result) {
         //     _this.model = result;
         //     _this.initCharts();
         // });
+        // _this.initCharts();
     }
 
-    // initCharts() {
-    //     var perMWin = this.model.matchesWon * 100 / this.model.totalMatches;
-    //     this.initChart("#ct-chart-m", [perMWin, 100 - perMWin]);
-    //     var perRWin = this.model.roundsWon * 100 / this.model.totalRounds;
-    //     this.initChart("#ct-chart-r", [perRWin, 100 - perRWin]);
-    //     var perRSnapped = this.model.roundsSnapped * 100 / this.model.totalRounds;
-    //     this.initChart("#ct-chart-rs", [perRSnapped, 100 - perRSnapped]);
-    // }
+    initCharts() {
+        var perMWin = this.model.matchesWon * 100 / this.model.totalMatches;
+        this.initChart("#ct-chart-m", [perMWin, 100 - perMWin]);
+        var perRWin = this.model.roundsWon * 100 / this.model.totalRounds;
+        this.initChart("#ct-chart-r", [perRWin, 100 - perRWin]);
+        var perRSnapped = this.model.roundsSnapped * 100 / this.model.totalRounds;
+        this.initChart("#ct-chart-rs", [perRSnapped, 100 - perRSnapped]);
+    }
 
-    // initChart(obj, series) {
-    //     if ($(obj).length) {
-    //         this.charts.pieChartRS = Chartist.Pie(obj, {
-    //             series: series
-    //         }, {
-    //                 donut: true,
-    //                 donutWidth: 30,
-    //                 startAngle: 270,
-    //                 total: 200,
-    //                 showLabel: false,
-    //                 chartPadding: 0,
-    //             });
-    //     }
-    // }
+    initChart(obj, series) {
+        if ($(obj).length) {
+            this.charts.pieChartRS = Chartist.Pie(obj, {
+                series: series
+            }, {
+                    donut: true,
+                    donutWidth: 30,
+                    startAngle: 270,
+                    total: 200,
+                    showLabel: false,
+                    chartPadding: 0,
+                });
+        }
+    }
 }
